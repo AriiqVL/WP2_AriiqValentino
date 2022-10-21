fc56a4f0cdfa74dcaff9eaff0f049118a27dad8a
AriiqVL <ariiqtino@gmail.com>
2022-10-21 16:59:11 +0700

    Latihan Pertemuan 7

---
+++ application/views/v_about.php
@@ -0,0 +1,29 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama :</h4>
+    <ul type="disc">
+        <li>Nama Depan : Fattahillah</li>
+        <li>Nama Belakang : Wahyu</li>
+        <li>Nama Panggilan : Fattah</li>
+    </ul>
+
+    <h4>Alamat :</h4>
+    <ul type="disc">
+    <li> Panggang Welut, Taji, Juwiring, Klaten</li>
+    </ul>
+    <h4>Hobi :</h4>
+    <ul type="disc">
+        <li>Bermain Game</li>
+    </ul>
+
+    <h4>Olah Raga Favorit :</h4>
+    <ul type="square">
+        <li>Sepak Bola</li>
+        <li>Voli</li>
+    </ul>
+    <h4>Makanan Favorit :</h4>
+    <ul type="disc">
+        <li>Nasi Goreng</li>
+        <li>Fried Chicken</li>
+    </ul>
+</section>
